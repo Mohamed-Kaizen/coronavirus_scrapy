cb3f84cc870b3e6752de05c3a4bf2d767c73f82a
Mohamed Nesredin <m.n.kaizen@gmail.com>
2020-06-29 06:29:55 +0300

    drop heroku

--- poetry_export.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo "exporting...."
-poetry export --format requirements.txt > requirements.txt
-
-echo "exporting with development dependencies...."
-poetry export --format requirements.txt > requirements_dev.txt --dev
-
--- poetry_export.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# export poertry to requirements.txt
-echo "exporting...."
-poetry export --format requirements.txt > requirements.txt
-
-echo "exporting with development dependencies...."
-poetry export --format requirements.txt > requirements_dev.txt --dev
-
